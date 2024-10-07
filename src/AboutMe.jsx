cb5d8e52d4ed2b07f35c2c9b8e485f86c827e2bd
Diego <dieguoin.developer@gmail.com>
2024-10-07 13:13:26 +0200

    .

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -6,7 +6,7 @@ export function AboutMe(){
         <div>
             <h1>About me</h1>
             <PresentationCard/>
-            <p>I have knowledge in some programing languages and technologyes. Which I`m best are:</p>
+            <p>I have knowledge in some programing languages and technologies. Which I`m best are:</p>
             <ImageGrid/>
         </div>
     )
